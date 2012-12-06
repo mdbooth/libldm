0f1c043dacecef87cdb1e72f3eff9c75fb2e3a31
Richard W.M. Jones <rjones@redhat.com>
2012-12-06 13:44:13 +0000

    As a rule of thumb, every C file should begin with:   #include <config.h>
    Also add AC_USE_SYSTEM_EXTENSIONS to configure.ac so that config.h
    defines _GNU_SOURCE etc when necessary.

--- src/gpt.c
+++ src/gpt.c
@@ -15,6 +15,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <config.h>
+
 #include <endian.h>
 #include <iconv.h>
 #include <linux/fs.h>
--- src/ldmtool.c
+++ src/ldmtool.c
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#define _GNU_SOURCE
+#include <config.h>
 
 #include <dirent.h>
 #include <errno.h>
